78182a3b09fb3085c3c01a428bb72a583999e5a6
Konstantin Gromov <gromov.konst95@gmail.com>
2022-07-01 16:38:49 +0300

    changed initialization for first list

--- App.xaml.cs
+++ App.xaml.cs
@@ -53,31 +53,10 @@
             {
                 new ToDoList()
                 {
-                    Name = "Application Level",
+                    Name = "Orginize your ToDo List!",
                     Items = new List<IToDoItem>()
                     {
-                        new ToDoItem(){Text="ViewModel Implementation",IsCompleted=true},
-                        new ToDoItem(){Text="Serialization"},
-                    }
-                },
-                new ToDoList()
-                {
-                    Name = "Model Level",
-                    Items = new List<IToDoItem>()
-                    {
-                        new ToDoItem(){Text="Basic model",IsCompleted=true},
-                        new ToDoItem(){Text="Interface extraction",IsCompleted=true},
-                        new ToDoItem(){Text="Better ViewModel interaction"},
-                    }
-                },
-                new ToDoList()
-                {
-                    Name = "View Level",
-                    Items = new List<IToDoItem>()
-                    {
-                        new ToDoItem(){Text="Baic presentation of elements",IsCompleted=true},
-                        new ToDoItem(){Text="Propper binding and commands",IsCompleted=true},
-                        new ToDoItem(){Text="Better design and visualisation"},
+                        new ToDoItem(){Text="Add new lists and tasks to begin with!"},
                     }
                 }
             };
