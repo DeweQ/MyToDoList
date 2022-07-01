ee83c92da1dde2fc16cf98c36a452d1824afcaf4
Konstantin Gromov <gromov.konst95@gmail.com>
2022-07-02 00:28:07 +0300

    easier Interaction with editTextBox

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using MyToDoList.ViewModels;
+using System.Windows.Controls.Primitives;
 
 namespace MyToDoList;
 
@@ -11,5 +12,49 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
         this.DataContext = applicationViewModel;
+    }
+
+    private void EditTextBoxLostFocus(object sender, RoutedEventArgs e)
+    {
+        var parent = FindParent<ItemsControl>(sender as TextBox);
+        var button = FindChild<ToggleButton>(parent);
+        button.IsChecked = false;
+    }
+
+    private T FindParent<T>(DependencyObject child) where T : DependencyObject
+    {
+        var parent = VisualTreeHelper.GetParent(child);
+        if (parent == null)
+            return null;
+        if(parent is T)
+            return parent as T;
+        return FindParent<T>(parent);
+    }
+
+    private T FindChild<T>(DependencyObject parent) where T : DependencyObject
+    {
+        int count = VisualTreeHelper.GetChildrenCount(parent);
+        for(int i = count-1; i >=0; i--)
+        {
+            var child = VisualTreeHelper.GetChild(parent, i);
+            if (child is T)
+                return child as T;
+            else child = FindChild<T>(child);
+            if (child is T)
+                return child as T;
+        }
+        return null;
+    }
+
+    private void SelectAllText(object sender, KeyboardFocusChangedEventArgs e)
+    {
+        TextBox tb = sender as TextBox;
+        tb.SelectAll();
+    }
+
+    private void LoseFocusOnEnterKeyUp(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+            EditTextBoxLostFocus(sender, e); 
     }
 }
