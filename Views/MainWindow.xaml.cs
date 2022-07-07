37aa1b194c080b6cb679e16e4869f1c5a2c4864b
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-07-07 11:48:00 +0300

    Extracted find relative methods to static class

--- Views/MainWindow.xaml.cs
+++ Views/MainWindow.xaml.cs
@@ -16,8 +16,9 @@ public partial class MainWindow : Window
 
     private void EditTextBoxLostFocus(object sender, RoutedEventArgs e)
     {
-        var parent = FindParent<ItemsControl>(sender as TextBox);
-        var button = FindChildByName<ToggleButton>(parent,"EditButton");
+        
+        var parent = RelativeFinder.FindParent<ItemsControl>(sender as TextBox);
+        var button = RelativeFinder.FindChildByName<ToggleButton>(parent,"EditButton");
         button.IsChecked = false;
     }
 
@@ -33,54 +34,5 @@ public partial class MainWindow : Window
             EditTextBoxLostFocus(sender, e);
     }
 
-    private T FindParent<T>(DependencyObject child) where T : DependencyObject
-    {
-        var parent = VisualTreeHelper.GetParent(child);
-        if (parent == null)
-            return null;
-        if (parent is T)
-            return parent as T;
-        return FindParent<T>(parent);
-    }
-
-    private T FindParentByName<T>(DependencyObject child, string name) where T : DependencyObject
-    {
-        var parent = VisualTreeHelper.GetParent(child);
-        if (parent == null)
-            return null;
-        if (parent is T && 
-            (parent as Control).Name == name)
-            return parent as T;
-        return FindParentByName<T>(parent,name);
-    }
-
-    private T FindChild<T>(DependencyObject parent) where T : DependencyObject
-    {
-        int count = VisualTreeHelper.GetChildrenCount(parent);
-        for (int i = count - 1; i >= 0; i--)
-        {
-            var child = VisualTreeHelper.GetChild(parent, i);
-            if (child is T)
-                return child as T;
-            else child = FindChild<T>(child);
-            if (child is T)
-                return child as T;
-        }
-        return null;
-    }
-
-    private T FindChildByName<T>(DependencyObject parent,string name) where T : DependencyObject
-    {
-        int count = VisualTreeHelper.GetChildrenCount(parent);
-        for (int i = 0; i <count; i++)
-        {
-            var child = VisualTreeHelper.GetChild(parent, i);
-            if (child is T && (child as Control).Name == name)
-                return child as T;
-            else child = FindChildByName<T>(child,name);
-            if (child is T && (child as Control).Name == name)
-                return child as T;
-        }
-        return null;
-    }
+    
 }
---
+++ Views/RelativeFinder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyToDoList.Views
+{
+    public static class RelativeFinder
+    {
+        public static T FindParent<T>(DependencyObject child) where T : DependencyObject
+        {
+            var parent = VisualTreeHelper.GetParent(child);
+            if (parent == null)
+                return null;
+            if (parent is T)
+                return parent as T;
+            return FindParent<T>(parent);
+        }
+
+        public static T FindParentByName<T>(DependencyObject child, string name) where T : DependencyObject
+        {
+            var parent = VisualTreeHelper.GetParent(child);
+            if (parent == null)
+                return null;
+            if (parent is T &&
+                (parent as Control).Name == name)
+                return parent as T;
+            return FindParentByName<T>(parent, name);
+        }
+
+        public static T FindChild<T>(DependencyObject parent) where T : DependencyObject
+        {
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = count - 1; i >= 0; i--)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                if (child is T)
+                    return child as T;
+                else child = FindChild<T>(child);
+                if (child is T)
+                    return child as T;
+            }
+            return null;
+        }
+
+        public static T FindChildByName<T>(DependencyObject parent, string name) where T : DependencyObject
+        {
+            int count = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < count; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                if (child is T && (child as Control).Name == name)
+                    return child as T;
+                else child = FindChildByName<T>(child, name);
+                if (child is T && (child as Control).Name == name)
+                    return child as T;
+            }
+            return null;
+        }
+    }
+}
