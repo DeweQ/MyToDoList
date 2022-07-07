4c2e99718b45da8c760cccecfe4b24bb96a9fb56
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-07-07 13:44:25 +0300

    Trying to include dlls to output exe

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,6 +8,7 @@
 
         App()
         {
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
             InitializeComponent();
         }
 
@@ -19,6 +20,23 @@
             MainWindow window = new MainWindow(ViewModel);
             app.Run(window);
             SerializeViewModel(ViewModel);
+
+        }
+
+        System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            string dllName = args.Name.Contains(',') ? args.Name.Substring(0, args.Name.IndexOf(',')) :
+                args.Name.Replace(".dll", "");
+            dllName = dllName.Replace(".", "_");
+
+            if (dllName.EndsWith("_resources")) return null;
+
+            System.Resources.ResourceManager rm = new(GetType().Namespace + ".Properties.Resources",
+                System.Reflection.Assembly.GetExecutingAssembly());
+
+            byte[] bytes = (byte[])rm.GetObject(dllName);
+            return System.Reflection.Assembly.Load(bytes);
+
         }
 
 
