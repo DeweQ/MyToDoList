b841b6c17610183beb47203c66301de41fdcfd24
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-07-07 14:00:58 +0300

    included through costura.fody

--- App.xaml.cs
+++ App.xaml.cs
@@ -8,7 +8,6 @@
 
         App()
         {
-            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(CurrentDomain_AssemblyResolve);
             InitializeComponent();
         }
 
@@ -22,23 +21,6 @@
             SerializeViewModel(ViewModel);
 
         }
-
-        System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
-        {
-            string dllName = args.Name.Contains(',') ? args.Name.Substring(0, args.Name.IndexOf(',')) :
-                args.Name.Replace(".dll", "");
-            dllName = dllName.Replace(".", "_");
-
-            if (dllName.EndsWith("_resources")) return null;
-
-            System.Resources.ResourceManager rm = new(GetType().Namespace + ".Properties.Resources",
-                System.Reflection.Assembly.GetExecutingAssembly());
-
-            byte[] bytes = (byte[])rm.GetObject(dllName);
-            return System.Reflection.Assembly.Load(bytes);
-
-        }
-
 
         static ApplicationViewModel InitializeViewModel()
         {
